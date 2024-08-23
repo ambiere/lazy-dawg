4699a84a0155a011e2f33514582eb0069c2d864e
zhid0399123 <zhid0399123@gmail.com>
2024-08-23 19:58:00 +0300

    the script

---
+++ script.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+runLazygit() {
+	if command -v lazygit >/dev/null 2>&1; then
+		if [[ "$1" == "newSession" ]]; then
+			echo "[tmux: session] creating new session"
+			tmux new-session -s $session_name -c $dir lazygit
+			exit 0
+		else
+			echo "[tmux: window] creating new window"
+			tmux new-window lazygit
+			exit 0
+		fi
+	else
+		echo "command not found: lazygit"
+		exit 0
+	fi
+}
+
+#check if tmux is installed
+if command -V tmux >/dev/null 2>&1; then
+	#check if tmux is running
+	tmux_running=$(pgrep tmux)
+	if [[ -z $tmux_running ]]; then
+		dir=$(pwd)
+		base=$(basename $dir | tr . _)
+		branch=$(git symbolic-ref --short HEAD)
+		session_name=$base"_"$branch
+
+		#tmux isn't running
+		#create ne session instead
+		runLazygit "newSession"
+	else
+		#tmux is running
+		#create new window instead
+		runLazygit "newWindow"
+	fi
+else
+	echo "command not found: tmux"
+	exit 0
+fi
